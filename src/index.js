d827e5ae6f49a32631e500ffc84e6ccb240581ed
Vitaly-Khvisevich <68824186+Vitaly-Khvisevich@users.noreply.github.com>
2021-03-09 16:56:39 +0300

    feat: add func min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr){
+    return(arr&&arr.length)? Math.min.apply(null, arr): 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr){
+    return(arr&&arr.length)? Math.max.apply(null, arr) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr){
+let calc, summ=0;
+if (arr&&arr.length){
+ for(i=0; i<arr.length; i++){
+   summ=summ+arr[i];
+ }
+  return(calc=summ/arr.length);
+}
+else{
+  return(0);
+}
 }
